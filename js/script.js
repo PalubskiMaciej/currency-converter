f8376ae8289f063609cd93c2666e142a67be296b
Palubski Maciej <palubskimaciej@gmail.com>
2023-06-06 20:14:11 +0200

    message at the same currency added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 {
+
+
+
+
+
+
+
   const calculateResult = (startedCurrency, finalCurrency, amount) => {
     switch (startedCurrency) {
       case "PLN":
@@ -9,8 +16,8 @@
             return amount * 0.19;
           case "USD":
             return amount * 0.24;
-
         }
+        break;
       case "EUR":
         switch (finalCurrency) {
           case "PLN":
@@ -19,8 +26,8 @@
             return amount * 0.86;
           case "USD":
             return amount * 1.07;
-
         }
+        break;
       case "GBP":
         switch (finalCurrency) {
           case "PLN":
@@ -29,8 +36,8 @@
             return amount * 1.16;
           case "USD":
             return amount * 1.24;
-
         }
+        break;
       case "USD":
         switch (finalCurrency) {
           case "PLN":
@@ -39,33 +46,44 @@
             return amount * 0.93;
           case "GBP":
             return amount * 0.80;
-
         }
+        break;
     }
-  }
 
-  const updateResultText = (result, finalCurrency) => {
-    const resultElement = document.querySelector(".js-result");
-    resultElement.innerText = `Po przeliczeniu wyszło: ${(result.toFixed(2))} ${(finalCurrency)}`;
-  }
-
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    const startedCurrencyElement = document.querySelector(".js-startedCurrency");
-    const finalCurrencyElement = document.querySelector(".js-finalCurrency");
-    const startedCurrency = startedCurrencyElement.value;
-    const finalCurrency = finalCurrencyElement.value;
-    const amountElement = document.querySelector(".js-amountToConvert");
-    const amount = +amountElement.value;
-    const result = calculateResult(startedCurrency, finalCurrency, amount);
-    updateResultText(result, finalCurrency);
 
 
   }
+
+  const onFormSubmit = (startedCurrencyElement, finalCurrencyElement, resultElement) => {
+    return (event) => {
+      event.preventDefault();
+
+      const startedCurrency = startedCurrencyElement.value;
+      const finalCurrency = finalCurrencyElement.value;
+      const amountElement = document.querySelector(".js-amountToConvert");
+      const amount = +amountElement.value;
+
+      const result = calculateResult(startedCurrency, finalCurrency, amount);
+
+
+
+      if (startedCurrency.localeCompare(finalCurrency) === 0) {
+        resultElement.innerText = `Wybierz różne waluty`;
+      } else {
+        resultElement.innerText = `Po przeliczeniu wyszło: ${(result.toFixed(2))} ${(finalCurrency)}`;
+      }
+    };
+  };
+
   const init = () => {
     const form = document.querySelector(".js-form");
-    form.addEventListener("submit", onFormSubmit);
+    const startedCurrencyElement = document.querySelector(".js-startedCurrency");
+    const finalCurrencyElement = document.querySelector(".js-finalCurrency");
+    const resultElement = document.querySelector(".js-result");
+    form.addEventListener("submit", onFormSubmit(startedCurrencyElement, finalCurrencyElement, resultElement));
   };
 
   init();
 }
+
+
